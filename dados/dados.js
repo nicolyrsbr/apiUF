eb8a935e295606bd6ae84591537728a631324be5
nicolyrsbr <nicolyrsbr@gmail.com>
2025-08-19 09:08:58 -0400

    Coleção de Dados

---
+++ dados/dados.js
@@ -0,0 +1,139 @@
+const colecaoUf = [
+    {
+        id: 1,
+        uf: "AC",
+        nome: "Acre"
+    },
+    {
+        id: 2,
+        uf: "AM",
+        nome: "Amazonas"
+    },
+    {
+        id: 3,
+        uf: "AP",
+        nome: "Amapá"
+    },
+    {
+        id: 4,
+        uf: "AL",
+        nome: "Alagoas"
+    },
+    {
+        id: 5,
+        uf: "BA",
+        nome: "Bahia"
+    },
+    {
+        id: 6,
+        uf: "CE",
+        nome: "Ceará"
+    },
+    {
+        id: 7,
+        uf: "DF",
+        nome: "Distrito Federal"
+    },
+    {
+        id: 8,
+        uf: "GO",
+        nome: "Goiás"
+    },
+    {
+        id: 9,
+        uf: "MA",
+        nome: "Maranhão"
+    },
+    {
+        id: 10,
+        uf: "MG",
+        nome: "Minas Gerais"
+    },
+    {
+        id: 11,
+        uf: "MS",
+        nome: "Mato grosso do Sul"
+    },
+    {
+        id: 12,
+        uf: "Mt",
+        nome: "Mato Grosso"
+    },
+    {
+        id: 13,
+        uf: "PA",
+        nome: "Pará"
+    },
+    {
+        id: 14,
+        uf: "PB",
+        nome: "Paraibá"
+    },
+    {
+        id: 15,
+        uf: "PE",
+        nome: "Pernambuco"
+    },
+    {
+        id: 16,
+        uf: "PI",
+        nome: "Piauí"
+    },
+    {
+        id: 17,
+        uf: "PR",
+        nome: "Paraná"
+    },
+    {
+        id: 18,
+        uf: "RJ",
+        nome: "Rio de Janeiro"
+    },
+    {
+        id: 19,
+        uf: "RO",
+        nome: "Rondônia"
+    },
+    {
+        id: 20,
+        uf: "RR",
+        nome: "Roraima"
+    },
+    {
+        id: 21,
+        uf: "RS",
+        nome: "Rio Grande do Sul"
+    },
+    {
+        id: 22,
+        uf: "SP",
+        nome: "São Paulo"
+    },
+    {
+        id: 23,
+        uf: "SE",
+        nome: "Sergipe"
+    },
+    {
+        id: 24,
+        uf: "TO",
+        nome: "Tocantins"
+    },
+    {
+        id: 25,
+        uf: "SC",
+        nome: "Santa Catarina"
+    },
+    {
+        id: 26,
+        uf: "AP",
+        nome: "Amapá"
+    },
+    {
+        id: 27,
+        uf: "MG",
+        nome: "Minas Gerais"
+    }
+];
+
+export default colecaoUf;
