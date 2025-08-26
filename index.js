808bb2430fced205e5574ba1b7c022068847b1dc
nicolyrsbr <nicolyrsbr@gmail.com>
2025-08-26 07:49:23 -0400

    Adicionando tratamento de erro

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ app.get('/ufs', (req, res) => {
     res.json(colecaoUf);
 });
 
+/* 
 app.get('/ufs/:iduf', (req, res) => {
     const idUF = parseInt(req.params.iduf);
-    const uf =  colecaoUf.find(u => u.id === idUF);
+    const uf = colecaoUf.find(u => u.id === idUF);
     res.json(uf);
+});
+*/
+
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find(u => u.id === idUF);
+        if (!uf) {
+            mensagemErro = 'UF não encontrada';
+        }
+    } else {
+        mensagemErro = 'Requisição inválida';
+    }
+
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).json({ "erro": mensagemErro })
+    }
 });
 
 
@@ -18,3 +41,5 @@ app.listen(8080, () => {
     let data = new Date();
     console.log('Servidor iniciado na porta 8080 em: ' + data);
 });
+
+
